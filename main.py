c76f8dcf7c32cab4d8d2361eb1e2d900132d1ed8
ProjectsRO-hit <official.rohitsingh22@gmail.com>
2025-01-07 23:53:06 +0530

    changes

--- main.py
+++ main.py
@@ -10,7 +10,25 @@ screen.addshape(image)
 #setting turtle
 turtle = Turtle()
 turtle.shape(image)
+#setting up pandas
+data = pd.read_csv("state_coor.csv")
+all_states_list = data["state_name"].to_list()
+
+guessed_states = []
+
+while len(guessed_states) < 29:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/30 correct", prompt="What's another state's name?").lower()
+
+    if answer_state in all_states_list:
+        guessed_states.append(answer_state)
+        t = Turtle()
+        t.hideturtle()
+        t.penup()
+        t.color("yellow")
+        state_data = data[data["state_name"] == answer_state]
+        t.goto(state_data["x"].item(), state_data["y"].item())
+        t.write(answer_state, align="center", font=("Courier", 10, "bold"))
 
 
 
-screen.mainloop()
+screen.exitonclick()
